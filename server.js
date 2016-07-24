b71e6006ec8cba0a12b7a957d160b40c6d16fdc5
Christos <xr_xarmatzis@outlook.com>
2016-07-24 13:43:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var app = express();
 var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.write('<p>Hello from Node' </p>');
+res.write('<ul>');
+res.write('<li><label>bing: </label>'+ process.env.bing_key +' </li>');
+res.write('<li><label>google :</label>'+ process.env.google_key +' </li>');
+res.write('</ul>');
 });
 
 var server = app.listen(port, function () {
