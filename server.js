10f88f06b39a8d7b8ab85ef5695d884a9eca99cb
Christos <xr_xarmatzis@outlook.com>
2016-07-24 16:25:28 +0300

    a

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ var app = express();
 var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
-    res.write('<p>Hello from Azure </p>');
-res.write('<ul>');
-res.write('<li><label>bing_key:</label>'+ process.env.bing_key +' </li>');
-res.write('<li><label>google_key:</label>'+ process.env.google_key +' </li>');
-res.write('</ul>');
+    res.write('<p>Happy Clouding!</p>');
+    
 });
 
 var server = app.listen(port, function () {
